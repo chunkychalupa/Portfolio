f4b104b326b2937116e2e2240aa0b38c0ebea2db
chunkychalupa <57198879+chunkychalupa@users.noreply.github.com>
2020-04-17 15:50:08 -0700

    Added all files from repl.it

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+
+  <body>
+    <link href="index.html" rel="stylesheet" type="text/css" />
+    <?php echo '<p>Hello World! Here are all of my Links for Activity 2.1.4 [Catherine Mah; 7]</p>'; ?> 
+
+    <ul>
+      <li><a href="index.html">index.html</a></li>
+      </br>
+      <li><a href="Last_AboutMe.html">About Me</a></li>
+       </br>
+      <li><a href="Last_Projects.html">Projects</a></li>
+       </br>
+      <li><a href="Last_Resume.html">Resume</a></li>
+       </br>
+      <li><a href="Last_ContactMe.html">Contact Me</a></li>
+       </br>
+    </ul>
+
+  </div>
+   </div>
+  </body>
+</html>
